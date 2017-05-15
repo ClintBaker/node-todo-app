dbaee7d7da48dc329c2b1ab5cace59db833d3917
Clint Baker <clintonjamesbaker@gmail.com>
2017-05-14 23:43:24 -0600

    Hash user password before saving

--- playground1/hashing.js
+++ playground1/hashing.js
@@ -1,15 +1,31 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = '123abc!';
 
-var token = jwt.sign(data, '123abc');
-console.log(token);
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
 
-var deco = jwt.verify(token, '123abc');
-console.log(`Decoded:`, deco);
+var hashedPassword = '$2a$10$ta0mzaKBFU25QMUT6vMdreVeQp1IZ.ML/IvcpBNm1ItIxy.2Gub1e';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, '123abc');
+// console.log(token);
+//
+// var deco = jwt.verify(token, '123abc');
+// console.log(`Decoded:`, deco);
 
 
 
